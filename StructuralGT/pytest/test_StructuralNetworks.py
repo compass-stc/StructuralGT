953742e95581a20fbc73edfcc9797d7654a1fed7
Alain Kadar <alaink@umich.edu>
2022-04-26 22:34:46 -0400

    Add pytest tests and data

---
+++ StructuralGT/pytest/test_StructuralNetworks.py
@@ -0,0 +1,13 @@
+from StructuralGT import network, base
+import pytest
+
+def test_find_node():
+    N = network.StructuralNetwork('pytest/Data/Rectangle1')
+    N.binarize()
+    N.stack_to_gsd()
+    N.G_u()
+    N.node_calc()
+    N.Node_labelling(N.Degree,'Degree','test.gsd')
+
+    nodeIDs_1 = np.where(np.asarray(N.Degree==4)) 
+
