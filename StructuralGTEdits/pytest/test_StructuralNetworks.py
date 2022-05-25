fd107c9aacb8f710bd20a7bb444f855cc3abbb50
Alain Kadar <alaink@umich.edu>
2022-05-25 19:57:32 -0400

    Add SGT

---
+++ StructuralGTEdits/pytest/test_StructuralNetworks.py
@@ -0,0 +1,15 @@
+from StructuralGTEdits import network, base
+import pytest
+
+def test_find_node():
+    """
+    N = network.StructuralNetwork('pytest/Data/Rectangle1')
+    N.binarize()
+    N.stack_to_gsd()
+    N.G_u()
+    N.node_calc()
+    N.Node_labelling(N.Degree,'Degree','test.gsd')
+
+    nodeIDs_1 = np.where(np.asarray(N.Degree==4)) 
+    """
+    pass
---
+++ StructuralGTEdits/setup.py~
@@ -0,0 +1,8 @@
+from setuptools import setup
+from Cython.Build import cythonize
+
+setup(
+    name='SGTE',
+    ext_modules=cythonize("convert.pyx"),
+    zip_safe=False
+)
