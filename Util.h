87b8b41a7b7f0a3b19ef7029192d85ab68e62433
alaink <alaink@CHEME-MAYBERRY.engin.umich.edu>
2023-03-13 12:13:49 -0400

    Move operator overloads

---
+++ Util.h
@@ -0,0 +1,27 @@
+#include <igraph.h>
+#include <vector>
+
+//Casts an igraph vector to a std::vector. The assignemnet operator may not 
+//be overloaded for a class that had been defined elsewhere. Hence I am using 
+//<<= instead.
+std::vector<float>& operator<<=(std::vector<float>& V, igraph_vector_t& I) {
+    for (int i=0; i<igraph_vector_size(&I); i++) {
+        V.push_back(VECTOR(I)[i]);
+    }
+    return V;
+}
+
+//Casts a square igraph matrix to a vector of std::vectors. 
+std::vector<std::vector<float> >& operator<<=(std::vector<std::vector<float> >& V, igraph_matrix_t& I) {
+    std::vector<float> _V(igraph_matrix_nrow(&I));
+    for (int i=0; i<igraph_matrix_nrow(&I); i++) {
+        for (int j=0; j<igraph_matrix_nrow(&I); j++) {
+            _V[j] = MATRIX(I,i,j);
+        }
+        V.push_back(_V);
+    }
+    return V;
+}
+
+
+
