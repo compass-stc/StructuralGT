ffe73b4b4edeafde57d4b1e0000e12cc42c159b9
AlainKadar <alaink@umich.edu>
2023-11-21 12:38:04 -0500

    Add ANC test

--- StructuralGT/pytest/test_AverageNodalConnectivity.py
+++ StructuralGT/pytest/test_AverageNodalConnectivity.py
@@ -3,23 +3,29 @@ from network_factory import fibrous
 
 import igraph as ig
 import pytest
-from networkx import average_node_connectivity
 import numpy.testing as npt
-
+import numpy as np
 
 class TestAverageNodalConnectivity:
     def test(self):
         #Obtain a connected graph
         testNetwork = fibrous()
-        testGraph = testNetwork.graph.to_networkx()
 
         #Instantiate a compute module and run calculation
         ComputeModule = AverageNodalConnectivity()
         ComputeModule.compute(testNetwork)
 
+        #Compute ANC manually
+        vals=[]
+        for i in range(testNetwork.graph.vcount()):
+            for j in range(i+1, testNetwork.graph.vcount()):
+                val=testNetwork.graph.vertex_connectivity(source=j,
+                                                          target=i,
+                                                          neighbors="ignore")
+                vals.append(val)
         #Check that the StructuralGT and networkx values are equal
         npt.assert_allclose(ComputeModule.average_nodal_connectivity,
-                            average_node_connectivity(testGraph),
-                            rtol=1e-2
+                            np.mean(np.asarray(vals)),
+                            rtol=1e-2,
                             )
 
