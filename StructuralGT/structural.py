c635a4888415bbb26284061355eb5199c9916e82
AlainKadar <alaink@umich.edu>
2024-02-06 16:49:53 -0500

    Latex formatting

--- StructuralGT/structural.py
+++ StructuralGT/structural.py
@@ -9,7 +9,7 @@ class Structural(_Compute):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def compute(self, network, skips=None):
+    def compute(self, network):
         """Calculates graph diameter, density, assortativity by degree,
         and average clustering coefficient. Also calculates per node degree,
         closeness, betweenness, and clustering.
@@ -64,23 +64,24 @@ class Structural(_Compute):
 
         .. math::
 
-        \rho = \frac{2e}{n(n-1)}
+           \rho = \frac{2e}{n(n-1)}
 
         """
         return self.Density
 
     @_Compute._computed_property
     def clustering(self):
-        r""":class:`np.ndarray`: Array of clustering coefficients, $\delta_i$s.
-        The clustering coefficient is the fraction of neighbors of a node that
-        are directly connected to each other as well (forming a triangle):
+        r""":class:`np.ndarray`: Array of clustering coefficients,
+        :math:`\delta_i`s. The clustering coefficient is the fraction of
+        neighbors of a node that are directly connected to each other as well
+        (forming a triangle):
 
         .. math::
 
-        \delta_i = \frac{2*T_i}{k_i(k_i-1)}
+           \delta_i = \frac{2*T_i}{k_i(k_i-1)}
 
-        Where $T_i$ Ti is the number of connected triples (visually triangles)
-        on node i.
+        Where :math:`T_i` is the number of connected triples (visually triangles)
+        on node :math:`i`.
 
         """
         return self.Clustering
@@ -91,7 +92,7 @@ class Structural(_Compute):
 
         .. math::
 
-        \Delta = \frac{\sum_i{\delta}}{n}
+           \Delta = \frac{\sum_i{\delta}}{n}
 
         """
         return np.mean(self.clustering)
@@ -102,49 +103,49 @@ class Structural(_Compute):
         connections by node degree. This value approaches 1 if nodes with the
         same degree are directly connected to each other and approaches −1
         if nodes are all connected to nodes with different degree. A value
-        near 0 indicates random orientation: :cite:`Newman2002`
+        near 0 indicates uncorrelated connections: :cite:`Newman2018`
 
         .. math::
 
-        r = \frac{1}{\sigma_q^2}\sum_{jk} jk(e_{jk}-q_j * q_k)
+           r = \frac{1}{\sigma_q^2}\sum_{jk} jk(e_{jk}-q_j * q_k)
 
-        where $q$ is the *remaining degree distribution*, $\sigma_{q}^2$ is its
-        variance. $e_{jk}$ is the joint probability distribution of the
-        remaining degrees of the two vertices at either end of a randomly
-        chosen edge.
+        where :math:`q` is the *remaining degree distribution*,
+        :math:`\sigma_{q}^2` is it variance. :math:`e_{jk}` is the joint
+        probability distribution of the remaining degrees of the two vertices
+        at either end of a randomly chosen edge.
 
         """
         return self.Degree_Assortativity
 
     @_Compute._computed_property
     def betweenness(self):
-        r""":class:`np.ndarray`: The betweenness centrality of node $i$ is a measure of how
-        frequently the shortest path between other nodes $u$ and $v$ pass 
-        through node $i$:
+        r""":class:`np.ndarray`: The betweenness centrality of node :math:`i` is a measure of how
+        frequently the shortest path between other nodes :math:`u` and :math:`v` pass 
+        through node :math:`i`:
 
         .. math::
 
-        C_{B}(i) = \sum_{u,v}\frac{\sigma(u,v)}{\sigma(u,v \parallel i)}
+           C_{B}(i) = \sum_{u,v}\frac{\sigma(u,v)}{\sigma(u,v \parallel i)}
         
         where $\sigma(u,v)$ represents the number of shortest 
-        paths that exist between nodes $u$ and $v$, with the term
+        paths that exist between nodes :math:`u` and :math:`v`, with the term
         $\sigma(u,v \parallel i)$ representing the number of those paths that
-        pass through node $i$. For variations on this parameter, see the
+        pass through node :math:`i`. For variations on this parameter, see the
         :class:`Betweenness` module.
         """
         return np.asarray(self.Betweenness)/(self.vcount-1)/(self.vcount-2)*2
 
     @_Compute._computed_property
     def closeness(self):
-        r""":class:`np.ndarray`: The closeness centrality of node $i$ is the
-        reciprocal of the average shortest distance from node $i$ to all 
+        r""":class:`np.ndarray`: The closeness centrality of node :math:`i` is the
+        reciprocal of the average shortest distance from node :math:`i` to all 
         other nodes:
 
         .. math::
 
-        C_{C}(i) = \frac{n-1}{\sum_{j=1}^{n-1}L(i,j)}
+           C_{C}(i) = \frac{n-1}{\sum_{j=1}^{n-1}L(i,j)}
 
-        where $L(i,j)$ is the shortest path between nodes $i$ and $j$. 
+        where $L(i,j)$ is the shortest path between nodes :math:`i` and :math:`j`. 
 
         """
         return self.Closeness
