7ac7420c50e3a3cbe6a1b851fc814756f4e9ae48
AlainKadar <alaink@umich.edu>
2024-04-11 10:19:51 -0400

    Add image processing options

---
+++ StructuralGT/pytest/options.py
@@ -0,0 +1,18 @@
+anf = {"Thresh_method":0, "gamma": 3, "md_filter": 0, "g_blur": 1, 
+            "autolvl": 0,"fg_color":0,"laplacian": 0, "scharr": 0, "sobel":0 ,
+            "lowpass": 1, "asize":7, "bsize":3, "wsize":5, "thresh": 103}
+
+agnwn = {"Thresh_method":0, "gamma": 3, "md_filter": 0, "g_blur": 1, 
+            "autolvl": 0,"fg_color":0,"laplacian": 0, "scharr": 0, "sobel":0 ,
+            "lowpass": 1, "asize":7, "bsize":3, "wsize":5, "thresh": 103}
+
+agnwn_high_thresh = {"Thresh_method":0, "gamma": 3, "md_filter": 0,
+                             "g_blur": 1, "autolvl": 0,"fg_color":0,
+                             "laplacian": 0, "scharr": 0, "sobel":0 ,
+                             "lowpass": 1, "asize":7, "bsize":3, "wsize":5,
+                             "thresh": 120}
+
+stick = {"Thresh_method":0, "gamma": 2, "md_filter": 0, "g_blur": 0,
+                 "autolvl": 0, "fg_color":0,"laplacian": 0,"scharr": 0,
+                 "sobel": 1,"lowpass": 0, "asize": 11,"bsize": 11,
+                 "wsize": 10, "thresh": 93}
