9f089962827562fe94ab1444fb9091784a59668a
AlainKadar <alaink@umich.edu>
2024-06-17 19:19:01 -0400

    Import binarizer without needing to specify name

--- StructuralGT/__init__.py
+++ StructuralGT/__init__.py
@@ -5,6 +5,8 @@ A python package for automated graph theory analysis
 of digital structural network images.
 """
 
+from .binarizer import Binarizer
+
 __version__ = "1.0.1b1"
 __author__ = "Drew Vecchio"
 __credits__ = "The Regents of the University of Michigan"
