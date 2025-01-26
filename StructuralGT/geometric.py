83b16645c7153108c0d0050a931af71966796340
AlainKadar <alaink@umich.edu>
2025-01-26 11:59:12 -0500

    Expose edge angles

--- StructuralGT/geometric.py
+++ StructuralGT/geometric.py
@@ -1,8 +1,9 @@
 from StructuralGT.util import _Compute
 import numpy as np
 import freud
+import warnings
 
-def LargestRotatingCrop(image_shape):
+def largest_rotating_crop(image_shape):
     """Returns the crop coordinates for the largest square that would remain 
     inside during any rotation of the image. Supports 2D images only.
 
@@ -25,6 +26,29 @@ def LargestRotatingCrop(image_shape):
     L4 = int(rotated_diagonal_length+L3)
     
     return [L1,L2,L3,L4]
+
+def vector_to_angle(vector):
+    """Converts 2D orientation vector to angle that the vector makes with
+    the x-axis, in degrees. Function range is 0 to 180 degrees.
+
+    Args:
+        vector (tuple): Orientation of a 2D edge
+
+    Returns:
+        angle (float): Angle that vector makes with the x-axis (i.e. 0th
+        dimension of the vector).
+    """
+
+    del_x, del_y = vector[1], vector[0]
+    if del_x==0 and del_y==0:
+        warnings.warn("Zero vector encountered. Returning 0")
+        return 0
+    if del_x==0: return 90
+    if del_y==0: return 0
+    if del_x*del_y>0:
+        return float(np.arctan(del_y/del_x)*180/np.pi)
+    if del_x*del_y<0:
+        return float(np.arctan(del_y/del_x)*180/np.pi + 180)
 
 class Nematic(_Compute):
     """Computes the nematic tensor of the graph. For details on how it 
@@ -51,6 +75,14 @@ class Nematic(_Compute):
         nematic.compute(_orientations[np.where(_orientations.any(axis=1))[0]])
         self._nematic = nematic
         self._orientations = _orientations
+        
+        angles = []
+        if network._2d:
+            for orientation in _orientations:
+                angles.append(vector_to_angle(orientation))
+            self._angles = angles
+        else:
+            self._angles = None
 
     @_Compute._computed_property
     def nematic(self):
@@ -60,9 +92,39 @@ class Nematic(_Compute):
         for more information."""
 
         return self._nematic
+
+    @_Compute._computed_property
+    def nematic_order_parameter(self):
+        r"""The nematic order parameter."""
+
+        return self.nematic.order
+
+    @_Compute._computed_property
+    def nematic_tensor(self):
+        r"""The nematic tensor."""
+
+        return self.nematic.nematic_tensor
+
+
+    @_Compute._computed_property
+    def director(self):
+        r"""The director."""
+
+        return self.nematic.director
     
     @_Compute._computed_property
     def orientations(self):
         r"""The edge orientations.""" 
 
         return self._orientations
+    
+    @_Compute._computed_property
+    def angles(self):
+        r"""The angles that each edge makes with the x-axis. Only supported
+        for 2D networks.
+        """
+
+        if self._angles is None:
+            raise TypeError("Angles are not calculated for 3D networks.")
+        else:
+            return np.array(list(map(vector_to_angle, self._orientations)))
