dc76762bf86997cdcd297387a0d3e905b327697c
AlainKadar <alaink@umich.edu>
2025-03-28 20:36:06 -0400

    License

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -6,31 +6,33 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'StructuralGT'
-copyright = '2023, Reagents of the University of Michigan'
-author = 'Alain Kadar'
-release = '2.0'
+project = "StructuralGT"
+copyright = "2023, Reagents of the University of Michigan"
+author = "Alain Kadar"
+release = "2.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autosummary","sphinxcontrib.bibtex",]
+extensions = [
+    "sphinx.ext.autosummary",
+    "sphinxcontrib.bibtex",
+]
 
 # For sphinxcontrib.bibtex (as of v2.0).
 bibtex_bibfiles = ["reference/StructuralGT.bib"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
 
-autodoc_member_order = 'bysource'
+autodoc_member_order = "bysource"
 autodoc_default_options = {
     "inherited-members": True,
     "show-inheritance": True,
