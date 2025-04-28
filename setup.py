6785ec49460cc0a28b1fb9b8e8d069b05c3ed742
AlainKadar <alaink@umich.edu>
2025-04-28 14:19:18 -0400

    Add apt_packages for read-the-docs

--- setup.py
+++ setup.py
@@ -20,12 +20,10 @@ with open('StructuralGT/metadata.json', 'w') as json_file:
     json.dump(metadata, json_file)
 
 if metadata['C_FLAG']:
-    if os.getenv("CONDA_PREFIX") is None:
-        PRE_PREFIX = os.getenv("MAMBA_ROOT_PREFIX")
-    else:
-        PRE_PREFIX = os.getenv("CONDA_PREFIX")
     if os.getenv("CONDA_ENVS_PATH") is not None:
         PRE_PREFIX = os.getenv("CONDA_ENVS_PATH")
+    else:
+        PRE_PREFIX = os.getenv("CONDA_PREFIX")
 
     if platform.system() == "Windows":
         PREFIX = os.path.join(PRE_PREFIX, "Library")
